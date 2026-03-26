2ae9b01be0bb1b70bc1db74425bc817bbacb6f27
wangfiox <wangfiox@gmail.com>
2026-03-26 20:57:33 +0800

    nix

--- flake.nix
+++ flake.nix
@@ -15,12 +15,10 @@
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
-        lib = pkgs.lib;
         toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
       in {
         devShell = pkgs.mkShell {
-          # 本机环境 (不保证可移植)
-          nativeBuildInputs = (with pkgs; [
+          packages = (with pkgs; [
             # pkg-config 用于管理: 编译、链接时所需库的路径
             pkg-config
             clang_16
@@ -28,15 +26,13 @@
             libxml2
           ]) ++ [
             # Mold Linker for faster builds (only on Linux)
-            (lib.optionals pkgs.stdenv.isLinux pkgs.mold)
+            (pkgs.lib.optionals pkgs.stdenv.isLinux pkgs.mold)
           ] ++ [
             # rust
-            # pkgs.rust-analyzer-unwrapped
             pkgs.cargo-insta
             toolchain
           ];
           buildInputs = with pkgs; [ pkgsCross.riscv64.buildPackages.gcc qemu ];
-          RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
         };
       });
 }
